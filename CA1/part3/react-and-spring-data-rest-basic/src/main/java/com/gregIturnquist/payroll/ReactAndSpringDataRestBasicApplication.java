5548f22d0bc1e888612832c751345bcaa7994e23
Joao <1241913@isep.ipp.pt>
2025-03-24 14:10:32 +0000

    feat: create part3 folder, switch to new tut-basic-gradle branch, and extract gradle Spring Boot project

---
+++ CA1/part3/react-and-spring-data-rest-basic/src/main/java/com/gregIturnquist/payroll/ReactAndSpringDataRestBasicApplication.java
@@ -0,0 +1,13 @@
+package com.gregIturnquist.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringDataRestBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndSpringDataRestBasicApplication.class, args);
+	}
+
+}
